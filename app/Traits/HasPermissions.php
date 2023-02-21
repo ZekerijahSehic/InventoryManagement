45d41bdb47014d307f53708b723e0483279a3b7a
Zekerijah <zekerijahsehic1@gmail.com>
2023-02-21 23:13:05 +0100

    Clean code indentation

--- app/Traits/HasPermissions.php
+++ app/Traits/HasPermissions.php
@@ -4,38 +4,35 @@ namespace App\Traits;
 
 use App\Models\Role;
 use App\Models\Permission;
-trait HasPermissions
-{
-    public function hasPermissionTo($permission)
-    {
+
+trait HasPermissions {
+
+    public function hasPermissionTo($permission) {
         return $this->hasPermissionThroughRole($permission) || $this->hasPermission($permission);
     }
-    public function refreshRole(Role $role)
-    {
+
+    public function refreshRole(Role $role) {
         $this->roles()->detach();
         $this->roles()->attach($role);
     }
-    public function hasPermissionThroughRole($permission)
-    {
-        foreach ($permission->roles as $role)
-        {
-            if($this->roles->contains($role))
-            {
+
+    public function hasPermissionThroughRole($permission) {
+        foreach ($permission->roles as $role) {
+            if($this->roles->contains($role)) {
                 return true;
             }
         }
         return false;
     }
-    public function roles()
-    {
+
+    protected function hasPermission($permission) {
+        return (bool) $this->permissions->where('name', $permission->name)->count();
+    }
+
+    public function roles() {
         return $this->belongsToMany(Role::class,'users_roles');
     }
-    public function permissions()
-    {
+    public function permissions() {
         return $this->belongsToMany(Permission::class,'users_permissions');
-    }
-    protected function hasPermission($permission)
-    {
-        return (bool) $this->permissions->where('name', $permission->name)->count();
     }
 }
